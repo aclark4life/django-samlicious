cb0ee62cd8859ac1ad412bc046fa518b58f4155d
Alex Clark <aclark@aclark.net>
2017-05-19 20:24:50 -0400

    Update

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*g8kw91#k0y#*yxqrdfqggjv(xl(q+w8ovxo^^ehgggt$h#3#k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
